b408f5d9211a43f63778fb4d2c9f17b2df339eb2
Jakub Plona <plonajakub@gmail.com>
2019-05-25 12:38:23 +0200

    DisjointSets rewritten.

--- structures/DisjointSets.cpp
+++ structures/DisjointSets.cpp
@@ -1,1 +1,31 @@
 #include "DisjointSets.h"
+
+DisjointSets::DisjointSets(int nVertex) {
+    sets = new int[nVertex];
+    size = nVertex;
+    for (int i = 0; i < nVertex; ++i) {
+        sets[i] = i;
+    }
+}
+
+DisjointSets::~DisjointSets() {
+    delete[] sets;
+}
+
+int DisjointSets::getSetRepresentative(int vertexID) {
+    return sets[vertexID];
+}
+
+bool DisjointSets::checkSetsDifferent(int startVertexID, int endVertexID) {
+    return sets[startVertexID] != sets[endVertexID];
+}
+
+void DisjointSets::unionSets(int startVertexID, int endVertexID) {
+    if (this->checkSetsDifferent(startVertexID, endVertexID)) {
+        for (int i = 0; i < size; ++i) {
+            if (sets[i] == sets[startVertexID]) {
+                sets[i] = sets[endVertexID];
+            }
+        }
+    }
+}
--- structures/DisjointSets.h
+++ structures/DisjointSets.h
@@ -3,56 +3,23 @@
 
 #include "graphs/misc/Edge.h"
 
-template<class T>
-class DSNode {
-public:
-    DSNode() = default;
-
-    DSNode(const T &up, int rank) : parent(up), rank(rank) {}
-
-    T parent;
-    int rank;
-};
-
-template<class T>
+// Disjoint sets of vertices
 class DisjointSets {
-private:
-    DSNode<T> *sets;
 public:
-    explicit DisjointSets(int dataCount) {
-        sets = new DSNode<T>[dataCount];
-    }
+    explicit DisjointSets(int nVertex);
 
-    ~DisjointSets() {
-        delete[] sets;
-    }
+    ~DisjointSets();
 
-    void makeSet(int vertexID) {
-        sets[vertexID].parent = vertexID;
-        sets[vertexID].rank = 0;
-    }
 
-    int findSet(int vertexID) {
-        if (sets[vertexID].parent != vertexID)
-            sets[vertexID].parent = findSet(sets[vertexID].parent);
-        return sets[vertexID].parent;
-    }
+    int getSetRepresentative(int vertexID);
 
-    void unionSets(Edge edge) {
-        int startVertexRoot, endVertexRoot;
+    bool checkSetsDifferent(int startVertexID, int endVertexID);
 
-        startVertexRoot = findSet(edge.startVertexID);
-        endVertexRoot = findSet(edge.endVertexID);
-        if (startVertexRoot != endVertexRoot) {
-            if (sets[startVertexRoot].rank > sets[endVertexRoot].rank)
-                sets[endVertexRoot].parent = startVertexRoot;
-            else {
-                sets[startVertexRoot].parent = endVertexRoot;
-                if (sets[startVertexRoot].rank == sets[endVertexRoot].rank)
-                    ++sets[endVertexRoot].rank;
-            }
-        }
-    }
+    void unionSets(int startVertexID, int endVertexID);
+
+private:
+    int *sets;
+    int size;
 };
 
 
